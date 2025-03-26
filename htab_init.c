ae7043168a3da3f9417dba0814e5ea9487d9a592
Andrii <and.klimenkoo@gmail.com>
2025-03-26 23:35:01 +0100

    implemented hash table's initialization

---
+++ htab_init.c
@@ -0,0 +1,24 @@
+// File: htab_init.c
+// Subject: IJC
+// Project: #2
+// Author: Andrii Klymenko, FIT VUT
+// Login: xklyme00
+// Date: 26.3.2025
+// Compiled: gcc version 11.4.0
+
+#include "htab.h"
+#include "htab_private.h"
+#include <stdlib.h>
+
+htab_t* htab_init(size_t n)
+{
+    htab_t* htab = (htab_t*) calloc(1, sizeof(htab_t) + n * sizeof(htab_pair_t*));
+
+    if(htab)
+    {
+        htab->arr_size = n;
+    }
+
+    return htab;
+}
+
