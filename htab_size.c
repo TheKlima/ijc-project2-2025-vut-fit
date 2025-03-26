7355c30a9448f7582aa7e2108a9d66a01e222110
Andrii <and.klimenkoo@gmail.com>
2025-03-26 23:43:53 +0100

    implemented hash table's 'getter' htab_size

---
+++ htab_size.c
@@ -0,0 +1,19 @@
+// File: htab_size.c
+// Subject: IJC
+// Project: #2
+// Author: Andrii Klymenko, FIT VUT
+// Login: xklyme00
+// Date: 26.3.2025
+// Compiled: gcc version 11.4.0
+
+#include <assert.h>
+
+#include "htab.h"
+#include "htab_private.h"
+#include <stdlib.h>
+
+size_t htab_size(const htab_t* t)
+{
+    assert(t);
+    return t->size;
+}
